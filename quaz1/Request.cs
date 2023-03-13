f445762b1a878034a9c98efa2db8827158d24740
HP <HP@DESKTOP-LF6N3L9>
2023-03-14 00:07:46 +0300

    Add project files.

---
+++ quaz1/Request.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.Xml.Linq;
+
+namespace quaz1
+{
+    public class Request
+    {
+        public string Id { get; }
+        public string URL { get; set; }
+
+        public void Print() => Console.WriteLine($"ID: {Id}, {URL} ");
+    }
+}
